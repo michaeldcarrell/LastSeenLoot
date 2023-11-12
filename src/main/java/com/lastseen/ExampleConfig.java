e51a3634c829facdb1d40a5cd492fd303e10524c
Michael Carrell <mcarrell@rentpath.com>
2023-11-12 08:33:16 -0700

    Functional model for developing Plugin

--- src/main/java/com/lastseen/ExampleConfig.java
+++ src/main/java/com/lastseen/ExampleConfig.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.lastseen;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
--- src/main/java/com/lastseen/ExamplePlugin.java
+++ src/main/java/com/lastseen/ExamplePlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.lastseen;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
